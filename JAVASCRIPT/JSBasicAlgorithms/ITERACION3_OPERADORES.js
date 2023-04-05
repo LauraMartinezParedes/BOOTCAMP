08ad34a44b4359de78d9c995db721d3a3028b162
Laura Martínez <you@example.com>
2023-04-05 18:18:33 +0200

    nuevos ejercicios

--- JAVASCRIPT/JSBasicAlgorithms/ITERACION3_OPERADORES.js
+++ JAVASCRIPT/JSBasicAlgorithms/ITERACION3_OPERADORES.js
@@ -4,7 +4,8 @@ console.log(10*5)
 //1.2 Divide 10 por 2 y muestra el resultado en un console.
 console.log(10/2)
 
-//1.3
+//1.3 Muestra mediante un console el resto de dividir 15 por 9.
+console.log(15/9)
 
 //1.4 Usa el correcto operador de asignación que resultará en o = 15, teniendo dos variables p = 10 y j = 5
 let p = 10
---
+++ JAVASCRIPT/JSFunctions/ITERACION1_buscar_max.js
@@ -0,0 +1,11 @@
+//1. buscar el maximo
+function sum(numberOne , numberTwo) {
+    if(numberOne > numberTwo) {
+        console.log(numberOne)
+    } else {
+        console.log(numberTwo)
+    }
+  }
+
+  sum(3 , 6)
+
